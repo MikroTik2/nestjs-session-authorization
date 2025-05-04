3f140904e8e11465128f59076dccc9b6dcf11983
Artur <111369356+MikroTik2@users.noreply.github.com>
2025-05-04 14:50:29 +0300

    chore: project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,77 @@
+// @ts-check
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+
+      '@typescript-eslint/no-misused-new': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      'prefer-promise-reject-errors': 'off',
+      'class-methods-use-this': 'off',
+      'no-useless-constructor': 'off',
+      'import/no-unresolved': 'off',
+      'no-control-regex': 'off',
+      'no-shadow': 'off',
+      'import/no-cycle': 'off',
+      'consistent-return': 'off',
+      'no-underscore-dangle': 'off',
+      'max-classes-per-file': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/prefer-promise-reject-errors': 'off',
+      'require-await': 'off',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'LabeledStatement',
+          message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+        },
+        {
+          selector: 'WithStatement',
+          message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+        },
+        {
+          selector: "MethodDefinition[kind='set']",
+          message: 'Property setters are not allowed',
+        },
+      ],
+    },
+  },
+);
